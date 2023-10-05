11bafe75c7e4e8d68cb2d681b96b1f027d713f23
Marshall <marshallmc@tamu.edu>
2023-10-04 22:26:38 -0500

    Added support for a pause menu Includes emitting an event on pause That can be used with other objects

--- GameManager.cs
+++ GameManager.cs
@@ -1,25 +1,36 @@
-using System.Reflection.Metadata;
-using System.Security.Cryptography.X509Certificates;
+using System.Reflection;
 using Godot;
 
 public partial class GameManager : Node
 {
-	// Called when the node enters the scene tree for the first time.
+	//signals
+	[Signal]
+	public delegate void ToggleGamePausedEventHandler(bool is_paused);
+
+
+
+	//note that public getters/setters means this is basically a global variable
+	private bool _game_paused = false;
+	public bool game_paused
+	{
+		get => _game_paused;
+		set {
+			_game_paused = value;
+			GetTree().Paused = _game_paused;
+			EmitSignal("ToggleGamePaused", _game_paused);
+		}
+	}
+	//Public member functions0
 	public override void _Ready()
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
 
 	public  override void _Input(InputEvent @event){
-		if(@event.IsActionPressed("ui_cancel")){
-			GetTree().Paused = !GetTree().Paused;
-
-		}
-
-
+		if(@event.IsActionPressed("ui_cancel"))
+			game_paused = !game_paused;
 	}
 }
--- Scenes/GameManaging/PauseMenu.cs
+++ Scenes/GameManaging/PauseMenu.cs
@@ -2,14 +2,28 @@ using Godot;
 
 public partial class PauseMenu : Control
 {
+	[Export]
+	GameManager game_manager;
+
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Hide();
+		game_manager.ToggleGamePaused += _OnGameManagerTogglePause;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+
+	public void _OnGameManagerTogglePause(bool is_paused){
+		if(is_paused)
+			Show();
+		else
+			Hide();
+	}
+
 }
